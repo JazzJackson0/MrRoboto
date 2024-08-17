25fe0d0b0638dae48f02d45f4948bbe1d6eb1a42
Jazz Jackson <jazzjacksonhomebase@gmail.com>
2024-08-17 12:18:18 -0400

    Made temporary workable functions

--- include/PathUtil.hpp
+++ include/PathUtil.hpp
@@ -2,18 +2,20 @@
 #include <iostream>
 #include <vector>
 #include </usr/include/eigen3/Eigen/Dense>
+#include "MapBuilder.hpp"
 using namespace Eigen;
 
 class PathUtil {
 
     private:
+        MapBuilder *map_builder;
 
 
     public:
 
         PathUtil();
 
-        std::vector<VectorXf> SamplePath(std::vector<VectorXi> path);
+        std::vector<VectorXi> SamplePath(std::vector<VectorXi> path);
 
         std::vector<VectorXf> SmoothPath(std::vector<VectorXi> path);
 };
